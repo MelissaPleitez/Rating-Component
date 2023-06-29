bac4b37ca9c3f63668df5549118db79db429cf92
MelissaPleitez <melissa.ramirez0pleitez@gmail.com>
2023-06-29 14:37:36 -0600

    adding confirmation alert

--- js/main.js
+++ js/main.js
@@ -1,46 +1,66 @@
-const form = document.querySelector('#form a')
-const btns = document.querySelectorAll('.btn-rounded')
+const form = document.querySelector('#form ')
+const btns = document.querySelectorAll('#form .btn-rounded')
+
+form.addEventListener('submit', sending)
+
+function BNTS (){}
+const btn = new BNTS ()
 
 
 
 
-btns.forEach(btn =>{
-    btn.addEventListener('click', function (e){
+btns.forEach((btnt, i) =>{
+    btnt.addEventListener('click', function (e){
         e.preventDefault()
-         
-        if(btn.classList.contains('active')){
-            btn.classList.remove('active')
-    
-            alerts(message, type)
-           return
-        }
-    
-        btn.classList.add('active')
-    
-        console.log('seleccionando..')
+        
+        btns.forEach((btnt, index)=>{
+
+            console.log(i, index)
+            i >= index ? btnt.classList.add('active') :
+            btnt.classList.remove('active')
+        })
+
     })
 })
 
 
-function alerts(message, type){
 
+function sending(e){
+e.preventDefault()
+btn.alerts('Your feedback has been sent it ', 'good')
 
-const container= document.querySelector('#message')
-const alert_message= document.createElement('p')
-
-
-if(type==='error'){
-
-    alert_message.classList.add('error')
-    alert_message.innerText= message
-    container.appendChild(alert_message)
-
-}else{
-    alert_message.classList.add('good')
-    alert_message.innerText= message
-    container.appendChild(alert_message)
-}
-
-
+console.log('seleccion..')
 
 }
+
+
+BNTS.prototype.alerts = function(message, type){
+    const container= document.querySelector('#message')
+    const alert_message= document.createElement('p')
+    const icons = document.createElement('i')
+    
+    if(type==='error'){
+        
+        alert_message.classList.add('error')
+        alert_message.innerText= message
+        icons.classList.add('bi','bi-x-circle')
+        alert_message.appendChild(icons)
+        container.appendChild(alert_message)
+    
+    }else{
+        
+        alert_message.classList.add('good')
+        alert_message.innerText= message
+        icons.classList.add('bi','bi-check-circle')
+        alert_message.appendChild(icons)
+        container.appendChild(alert_message)
+    }
+    setTimeout(() => {
+        alert_message.remove()
+    
+    }, 3000);
+    
+    
+
+}
+
